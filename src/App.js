d0bba985589f4790867451a1dcebabe617402b1d
Tushar Jain <tushar.26tj@gmail.com>
2025-11-29 20:49:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [minutes, setMinutes] = useState(0);
+  const [sec, setSec] = useState("00");
+  const [num, setNum] = useState(0);
+  const [start, setStart] = useState(false);
+  const stopWatch = () => {
+    return;
+    // if (sec >= 59) {
+    //   setMinutes++;
+    // } else {
+    //   setNum(num++);
+    //   setSec(`${num}`);
+    // }
+  };
+  const reset = () => {
+    setMinutes(0);
+    setSec("00");
+    setNum(0);
+  };
+  while (start) {
+    stopWatch();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Stopwatch</h1>
+      <p>
+        Time: {minutes}:{sec}
+      </p>
+      {start ? (
+        <button onClick={() => setStart(false)}>Stop</button>
+      ) : (
+        <button onClick={() => setStart(true)}>Start</button>
+      )}
+      <button>Reset</button>
     </div>
   );
 }
